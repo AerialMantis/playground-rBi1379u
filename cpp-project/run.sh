02f238d8dbf65f1fa411abdde487568be1d9a4b7
Gordon <Gordon>
2018-02-19 01:30:59 +0000

    Add potential workaround to Cmake issue

--- cpp-project/run.sh
+++ cpp-project/run.sh
@@ -3,4 +3,4 @@
 mkdir build
 cd build
 
-cmake ../ -GNinja -DCMAKE_C_COMPILER="/usr/bin/gcc" -DOpenCL_INCLUDE_DIR="/opt/opencl_lib/incude" -DOpenCL_LIBRARY="/opt/opencl_lib/lib/libOpenCL.so" -DCOMPUTECPP_PACKAGE_ROOT_DIR="usr/local"
+cmake ../ -GNinja -DCMAKE_C_COMPILER="/usr/bin/gcc" -DOpenCL_INCLUDE_DIR="/opt/opencl_lib/incude" -DOpenCL_LIBRARY="/opt/opencl_lib/lib/libOpenCL.so" -DCOMPUTECPP_PACKAGE_ROOT_DIR="usr/local" -DCOMPUTECPP_RUNTIME_LIBRARY="usr/local/lib/libComputeCpp.so"
