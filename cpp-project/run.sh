6f766f68be7ac875995b4382ceb95afd2924e637
Gordon <Gordon>
2018-02-20 00:05:28 +0000

    Add ls to test for output files

--- cpp-project/run.sh
+++ cpp-project/run.sh
@@ -6,3 +6,5 @@ cd build
 export LD_LIRARY_PATH=$PATH:/opt/opencl_lib/lib:/usr/local/lib
 
 cmake ../ -GNinja -DCMAKE_C_COMPILER="/usr/bin/gcc" -DOpenCL_INCLUDE_DIR="/opt/opencl_lib/incude" -DOpenCL_LIBRARY="/opt/opencl_lib/lib/libOpenCL.so" -DCOMPUTECPP_PACKAGE_ROOT_DIR="/usr/local"
+
+ls
