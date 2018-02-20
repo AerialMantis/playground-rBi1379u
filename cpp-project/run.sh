ed981d0275ef977fb6485db9817682d2315d8ba9
Gordon <Gordon>
2018-02-20 00:29:34 +0000

    Test setting the OpenCL path

--- cpp-project/run.sh
+++ cpp-project/run.sh
@@ -5,9 +5,7 @@ cd build
 
 export LD_LIRARY_PATH=$PATH:/opt/opencl_lib/lib:/usr/local/lib
 
-# -DOpenCL_INCLUDE_DIR="/opt/opencl_lib/incude" -DOpenCL_LIBRARY="/opt/opencl_lib/lib/libOpenCL.so"
-
-cmake ../ -GNinja -DCMAKE_C_COMPILER="/usr/bin/gcc" -DCOMPUTECPP_PACKAGE_ROOT_DIR="/usr/local"
+cmake ../ -GNinja -DCMAKE_C_COMPILER="/usr/bin/gcc" -DOpenCL_INCLUDE_DIR="/opt/opencl_lib/incude" -DOpenCL_LIBRARY="/opt/opencl_lib/lib/libOpenCL.so" -DCOMPUTECPP_PACKAGE_ROOT_DIR="/usr/local"
 
 ninja
 
