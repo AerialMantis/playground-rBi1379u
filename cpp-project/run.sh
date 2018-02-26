211abb0834df6784d0eab33140ab5d7b8717d309
Gordon <Gordon>
2018-02-26 01:19:43 +0000

    Remove specifying OpenCL lib path explicitly work with new docer image.

--- cpp-project/run.sh
+++ cpp-project/run.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
 
+# -DOpenCL_INCLUDE_DIR="/opt/opencl_lib/incude" -DOpenCL_LIBRARY="/opt/opencl_lib/lib/libOpenCL.so"
+# export LD_LIRARY_PATH=$PATH:/opt/opencl_lib/lib:/usr/local/lib
+
 mkdir build
 cd build
 
-export LD_LIRARY_PATH=$PATH:/opt/opencl_lib/lib:/usr/local/lib
-
-cmake ../ -GNinja -DCMAKE_C_COMPILER="/usr/bin/gcc" -DOpenCL_INCLUDE_DIR="/opt/opencl_lib/incude" -DOpenCL_LIBRARY="/opt/opencl_lib/lib/libOpenCL.so" -DCOMPUTECPP_PACKAGE_ROOT_DIR="/usr/local"
+cmake ../ -GNinja -DCMAKE_CXX_COMPILER="/usr/bin/gcc" -DCOMPUTECPP_PACKAGE_ROOT_DIR="/usr/local/computecpp"
 
 ninja
-
-ls
