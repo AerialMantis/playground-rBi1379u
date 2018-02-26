2b37cde3f4226155f96f5887630032609a39df1a
Gordon <Gordon>
2018-02-26 17:56:57 +0000

    Refactor frammework for running code samples.

--- cpp-project/run.sh
+++ cpp-project/run.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
-# export LD_LIRARY_PATH=$PATH:/opt/opencl_lib/lib:/usr/local/lib
+sample=$1
 
 mkdir build
 cd build
 
-cmake ../ -GNinja -DCMAKE_CXX_COMPILER="/usr/bin/g++" -DOpenCL_INCLUDE_DIR="$OCL_INC" -DOpenCL_LIBRARY="$OCL_LIB/libOpenCL.so" -DCOMPUTECPP_PACKAGE_ROOT_DIR="/usr/local/computecpp"
+cmake ../ -GNinja -DCMAKE_CXX_COMPILER="/usr/bin/g++" -DOpenCL_INCLUDE_DIR="$OCL_INC" -DOpenCL_LIBRARY="$OCL_LIB/libOpenCL.so" -DCOMPUTECPP_PACKAGE_ROOT_DIR="/usr/local/computecpp" -DSAMPLE_NAME=$sample
 
-ninja
+ninja $sample
+
+./$sample
