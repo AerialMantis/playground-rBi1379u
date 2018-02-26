167fe4d37d1f71dc88be11ecc76166a13828a1b4
Gordon <Gordon>
2018-02-26 22:25:53 +0000

    Add fix for bash script.

--- cpp-project/run.sh
+++ cpp-project/run.sh
@@ -1,15 +1,11 @@
 #!/bin/sh
 
-@echo off
-
 sample=$1
 
 mkdir build
 cd build
 
-cmake ../ -GNinja -DCMAKE_CXX_COMPILER="/usr/bin/g++" -DOpenCL_INCLUDE_DIR="$OCL_INC" -DOpenCL_LIBRARY="$OCL_LIB/libOpenCL.so" -DCOMPUTECPP_PACKAGE_ROOT_DIR="/usr/local/computecpp" -DSAMPLE_NAME=$sample
-
-@echo on
+cmake ../ -GNinja -DCMAKE_CXX_COMPILER="/usr/bin/g++" -DOpenCL_INCLUDE_DIR="$OCL_INC" -DOpenCL_LIBRARY="$OCL_LIB/libOpenCL.so" -DCOMPUTECPP_PACKAGE_ROOT_DIR="/usr/local/computecpp" -DSAMPLE_NAME=$sample > /dev/null
 
 ninja $sample
 
