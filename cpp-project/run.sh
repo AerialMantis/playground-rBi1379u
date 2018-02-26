3698eb4c954d135c647cf7ebbc29886a422487ca
Gordon <Gordon>
2018-02-26 01:33:55 +0000

    Fix OpenCL lib path for new docker image.

--- cpp-project/run.sh
+++ cpp-project/run.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
 
-# -DOpenCL_INCLUDE_DIR="/opt/opencl_lib/incude" -DOpenCL_LIBRARY="/opt/opencl_lib/lib/libOpenCL.so"
 # export LD_LIRARY_PATH=$PATH:/opt/opencl_lib/lib:/usr/local/lib
 
 mkdir build
 cd build
 
-cmake ../ -GNinja -DCMAKE_CXX_COMPILER="/usr/bin/gcc" -DCOMPUTECPP_PACKAGE_ROOT_DIR="/usr/local/computecpp"
+cmake ../ -GNinja -DCMAKE_CXX_COMPILER="/usr/bin/gcc" -DOpenCL_INCLUDE_DIR="$OCL_INC" -DOpenCL_LIBRARY="$OCL_LIB" -DCOMPUTECPP_PACKAGE_ROOT_DIR="/usr/local/computecpp"
 
 ninja
